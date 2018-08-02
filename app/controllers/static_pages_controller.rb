ec7392fe8c55c4649fdd5c3ee2152c7235d383bf
Nicolas <nicolas.bertin0@gmail.com>
2018-08-02 17:44:32 +0200

    before actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,3 @@
 class StaticPagesController < ApplicationController
   before_action :logged_in_user, only: :secret
-
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_path
-     end
-   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  
+
   def index
   end
 
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(email: params[:user][:email], password_digest: params[:user][:password_digest])
+    @user = User.create(email: params[:user][:email], password: params[:user][:password_digest], password_confirmation: params[:user][:password_digest])
     log_in(@user)
     flash[:success] = "Bienvenue ma men!"
     redirect_to user_path id: @user.id
@@ -25,14 +25,21 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user = User.create(email: params[:user][:email], password_digest: params[:user][:password_digest])
-    redirect_to user_path id: user.id
+    @user = User.find(params[:id])
+    @user.update(email: params[:user][:email], password: params[:user][:password_digest], password_confirmation: params[:user][:password_digest])
+    redirect_to user_path id: @user.id
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
     redirect_to users_path
+  end
+
+  private
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
