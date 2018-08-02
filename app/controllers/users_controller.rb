ade92843a5df958a7405775bcf2a5f15601e5308
Ludovic Bourgoin <ludovic.bourgoin@gmail.com>
2018-08-02 12:22:52 +0200

    CRUD in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(email: params[:email], password_digest: params[:password_digest])
+    @user = User.create(email: params[:user][:email], password_digest: params[:user][:password_digest])
     redirect_to user_path id: @user.id
       end
 
@@ -16,9 +16,11 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.update(email: params[:email], passworddigest: params[:password_digest])
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  #has_secure_password
+  # validates :email, presence: true
+  # validates :email, uniqueness: true
+  # TODO: Fix validation issue
 end
