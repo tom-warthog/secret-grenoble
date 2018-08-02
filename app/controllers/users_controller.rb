3e21fac796b9a131971dfcdb21a3e66819b6a5a2
Ludovic Bourgoin <ludovic.bourgoin@gmail.com>
2018-08-02 13:25:24 +0200

    Added User CRUD + Bootstrap layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(email: params[:user][:email], password_digest: params[:user][:password_digest])
-    redirect_to user_path id: @user.id
+      redirect_to user_path id: @user.id
       end
 
   def show
@@ -20,9 +20,18 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.update(email: params[:email], passworddigest: params[:password_digest])
+    @user = User.find(params[:id])
+    @user = User.create(email: params[:user][:email], password_digest: params[:user][:password_digest])
+    redirect_to user_path id: @user.id
   end
 
   def destroy
+    @user = User.find(params[:id])
+    puts params
+    puts params
+    @user.destroy
+
+    redirect_to users_path
+
   end
 end
